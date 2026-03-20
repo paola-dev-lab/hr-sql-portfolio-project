0c06829b346451e301e904dbf1a2e98381f7972e
Paola P <dppuertol@gmail.com>
2026-03-20 12:22:33 +0000

    add absence par department analytics

--- hr_analysis.sql
+++ hr_analysis.sql
@@ -6,4 +6,20 @@ SELECT
     ROUND(AVG((CURRENT_DATE - hire_date) / 365.0), 1) AS anciennete_moyenne_employees
 FROM 
     employees;
+
+-- Nombre d'absences par département
+-- Cette requête permet de compter le volume total d'absences
+-- dans chaque département en reliant les tables absences,
+-- employees et departments.
+-- Résultat observé : le département Ressources Humaines
+-- est celui qui enregistre le plus grand nombre d'absences.
+SELECT 
+    department_name,
+    COUNT(absence_id) AS nombre_absences
+FROM absences
+INNER JOIN employees
+    ON absences.employee_id = employees.employee_id
+INNER JOIN departments
+    ON departments.department_id = employees.department_id
+GROUP BY department_name;
 	
